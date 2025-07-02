15a87df2a02e8cdb5efefd1a4f73e868e8a821b6
yonatan ayala <informaticaprofesional142@gmail.com>
2025-07-02 00:28:06 -0300

    se hizo una func para validar recibir los valores de los inputs para reutilizar en guardar_cambios(la cual ahora funciona) y agendar

--- contactos/routes.py
+++ contactos/routes.py
@@ -3,40 +3,50 @@ import db
 
 contactos = Blueprint('contactos',__name__)
 
+#funcion reutilizable para recibir datos de input y validarlos
+def campos(n,t,c,o):
+    input_campos = {
+        'nombre' : request.form.get(n).strip(),
+        'telefono' : request.form.get(t).strip(),
+        'correo' : request.form.get(c).strip(),
+        'obs' : request.form.get(o).strip()
+    }
+
+    errores = []
+    #new validacion campo por campo
+    if not input_campos['nombre'] :
+        errores.append("El nombre es Obligario!")
+    if not input_campos['telefono'].isdigit() or len(input_campos['telefono']) < 10:
+        errores.append("Numero de telefono invalido.")
+    if not input_campos['correo'] or '.' not in input_campos['correo'].split('@')[-1]:
+        errores.append("La direccion de correo es invalida.")
+
+    if errores:
+        for e in errores:
+            flash(e)
+
+    return input_campos,errores
+
+
 #agendar contactos
 @contactos.route('/agendar', methods=['GET','POST'])
 def agendar():
     if 'id' in session:
         if request.method == 'POST':
-            nombre = request.form.get('nombre').strip()
-            telefono = request.form.get('telefono').strip()
-            correo = request.form.get('correo').strip()
-            observacion = request.form.get('observacion').strip()
-
-            errores = []
-            #new validacion campo por campo
-            if not nombre:
-                errores.append("El nombre es Obligario!")
-            if not telefono.isdigit() or len(telefono) < 10:
-                errores.append("Numero de telefono invalido.")
-            if not correo or '.' not in correo.split('@')[-1]:
-                errores.append("La direccion de correo es invalida.")
-
-            #mostrar errores si hay
-            if errores:
-                for error in errores:
-                    flash(error)
-                return redirect(url_for('contactos.agendar'))
+            valor_c,err = campos('nombre','telefono','correo','observacion')
             
+            if err:
+                return redirect(url_for('contactos.agendar'))
+
             id_user = session['id']
             conexion = db.conexion_db()
             cursor = conexion.cursor()
             cursor.execute("INSERT INTO contacto (nombre, telefono, email, observaciones, id_usuario) VALUES (%s, %s, %s, %s, %s)",
-                           (nombre, telefono, correo, observacion, id_user))
+                           (valor_c['nombre'], valor_c['telefono'], valor_c['correo'], valor_c['obs'], id_user))
             conexion.commit()
             conexion.close()
             flash("Contacto agendado Exitosamente!! Wow")
-            return redirect(url_for('agendar'))
+            return redirect(url_for('contactos.agendar'))
         
         return render_template('contactos/agendar.html')
     
@@ -60,8 +70,19 @@ def editar(id):
         redirect(url_for('auth.loguear'))
 
 
-#agregar func para guardar los cambios de la edicion
+#func para guardar los cambios de la edicion
 @contactos.route('/guardar_cambios/<int:id>', methods=['POST'])
 def guardar_cambios(id):
-    flash("Todavia no esta lista esta Funcionalidad.")
-    return redirect(url_for('contactos.editar', id=id))
+    valor_c,err = campos('nombre','telefono','correo','observacion')
+
+    if err:
+        return redirect(url_for('contactos.editar',id=id))
+
+    conexion = db.conexion_db()
+    cursor = conexion.cursor()
+    cursor.execute('UPDATE contacto SET nombre=%s, telefono=%s, email=%s, observaciones=%s WHERE id_contacto=%s',
+                   (valor_c['nombre'],valor_c['telefono'],valor_c['correo'],valor_c['obs'],id))
+    conexion.commit()
+    conexion.close()
+    flash("Cambios guardados exitosamente!!")
+    return redirect(url_for('contactos.editar',id=id))
