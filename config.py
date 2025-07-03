c7efd92a82549dd4a05fda8a2e1f330f5ac1c664
yonatan ayala <informaticaprofesional142@gmail.com>
2025-07-03 20:03:44 -0300

    se cambio la contraseña de user de mySQL

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 #datos de la base de datos
 MYSQL_HOST = 'localhost'
 MYSQL_USER = 'flaskuser'
-MYSQL_PASSWORD = 'tu_contraseña'
-MYSQL_DATABASE = 'prueba'
+MYSQL_PASSWORD = 'abc123'
+MYSQL_DATABASE = 'myapp'
 SECRET_KEY = 'clave-secreta'
